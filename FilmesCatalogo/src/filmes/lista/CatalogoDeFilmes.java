4658bb0a64e342c28d694415a0f06d28e2515e8b
VitorIsacB <vitorisac14@gmail.com>
2024-08-17 15:37:19 -0300

    Conteudos adicionados, mas com alguns erros para corrigir.

--- FilmesCatalogo/src/filmes/lista/CatalogoDeFilmes.java
+++ FilmesCatalogo/src/filmes/lista/CatalogoDeFilmes.java
@@ -1,2 +1,30 @@
-package filmes.lista;public class CatalogoDeFilmes {
+package filmes.lista;
+
+import java.util.ArrayList;import java.util.List;
+
+public class CatalogoDeFilmes {
+    private List<Filme> filmes;
+
+    public CatalogoDeFilmes() {
+        this.filmes = new ArrayList<>();
+    }
+    public void adicionarFilme(Filme filme) {
+        filmes.add(filme);    }
+    public void listarFilmes() {
+        if (filmes.isEmpty()) {
+            System.out.println("Nenhum filme no catálogo.");
+            return;
+        }        for (Filme filme : filmes) {
+            System.out.println(filme);
+        }
+    }
+
+    public Filme buscarFilmePorTitulo(String titulo) {
+        for (Filme filme : filmes) {
+            if (filme.getTitulo().equalsIgnoreCase(titulo)) {
+                return filme;
+            }
+        }
+        return null;
+    }
 }
--- FilmesCatalogo/src/filmes/lista/Filme.java
+++ FilmesCatalogo/src/filmes/lista/Filme.java
@@ -1,2 +1,27 @@
-package filmes.lista;public class Filme {
+package filmes.lista;
+
+public class Filme {
+    private String titulo;
+    private String diretor;
+    private int ano;
+
+    public Filme(String titulo, String diretor, int ano) {
+        this.titulo = titulo;
+        this.diretor = diretor;
+        this.ano = ano;
+    }
+
+    public Filme(String titulo) {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getDiretor() {
+        return diretor;
+    }
+    public int getAno() {
+        return ano;
+    }
+
 }
